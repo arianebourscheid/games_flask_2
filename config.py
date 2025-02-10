7238b0672865b0c197bc0dc0e067b1ea2ba5a9b8
Ariane Bourscheid <arianebourscheid@gmail.com>
2025-02-10 12:03:59 -0300

    atualizando o banco de dados -  config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD = 'mysql+mysqlconnector',
         usuario = 'root',
-        senha = 'Mysql+97613',
+        senha = 'admin',
         servidor = 'localhost',
         database = 'games'
     )
