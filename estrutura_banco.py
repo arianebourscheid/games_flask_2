7afe4cdf080ab5bbb301e29900ceb2f28fc53c91
Ariane Bourscheid <arianebourscheid@gmail.com>
2025-02-10 12:04:41 -0300

    atualizando o banco de dados -  estrutura_banco.py

--- estrutura_banco.py
+++ estrutura_banco.py
@@ -8,7 +8,7 @@ try:
       conn = mysql.connector.connect(
             host='127.0.0.1',
             user='root',
-            password='Mysql+97613'
+            password='admin'
       )
 except mysql.connector.Error as err:
       if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
